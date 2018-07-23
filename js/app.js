82c2f27da4738769689aa0ff844122350e17f6b0
Sharina V. Jones <sharinavjones@gmail.com>
2018-07-23 11:59:59 -0400

    merging script files

--- js/app.js
+++ js/app.js
@@ -364,3 +364,168 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
   }  
 });
+
+$(function() {
+    let $menuBtn = $('#drop-btn');
+    const $menu = $('.drop-body');
+
+    // if ($window.width() < 768) {
+    //     $menu.css({'display': 'none'});
+    // }
+
+ $(window).resize(function () {
+        if ($(this).width() >= 768) {
+            $menu.show();
+        }
+    });
+
+    $menuBtn.on('click', function() {
+        $menu.slideToggle();
+
+    });
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+//accordian
+const $panels = $('.accordian li > p');
+let $currentP = $panels.first().text();
+$panels.hide();
+$panels.first().show();
+
+$('.accordian li > h3').on('click', function(e) {
+    let $nextLi = $(this).parent();
+    let $nextP = $nextLi.find('p');
+    if ($nextP.text() !== $currentP) { 
+        $panels.slideUp();       
+        $nextP.slideDown();
+        $currentP = $nextP.text();
+    }
+});
+
+ //modal
+ const $registration = $('.registration');
+const $modal = $('#modal');
+const $cancel = $('#modal #cancel');
+const $nextButton = $('#next').hide();
+
+$cancel.on('click', function() {
+    $modal.slideUp();
+    $modal.css(
+        {
+            "display": "none"
+        }
+    );
+});
+
+$registration.on('click', function() {
+    let screenTop = $(document).scrollTop();
+    $modal.slideDown();
+    $modal.css({
+        "top" : screenTop,
+        "display": "flex",
+        "flex-direction" : "column",
+        "justify-content" : "center"
+    });
+
+    const $formFields = $('#modal #form-body fieldset');
+    showForm($formFields);
+});
+
+function showForm($formFields) {
+    //hide the long version of the form
+    $formFields.hide();
+
+    //show the next button & hide submit
+    $nextButton.show();
+    $('button[id="submit"]').hide();
+
+    //show the first section
+    let currentSection = $formFields.first();
+    $formFields.first().show();
+
+    //when the next button is clicked
+        //validate the current section and show the next section
+    
+    $nextButton.on('click', function (e) {
+        let isValid = true;
+
+        if (currentSection.attr('class') === 'basic') {
+            isValid = checkName();
+            isValid = checkEmail();
+            if (isValid) {
+                currentSection.fadeOut('slow');
+                currentSection = currentSection.next();
+                currentSection.fadeIn('slow');
+            }
+        } else if (currentSection.attr('class') === 'shirt') {
+            //no validation required
+            currentSection.fadeOut('slow');
+            currentSection = currentSection.next();
+            $nextButton.attr('disabled', 'disabled');
+            $nextButton.css({"background-color": "gray"});
+            currentSection.fadeIn('slow');
+        } else if (currentSection.attr('class') === 'activities') {
+            currentSection.fadeOut('slow');
+            currentSection = currentSection.next();
+            $nextButton.hide();
+            $('button[id="submit"]').show();
+            currentSection.fadeIn('slow');           
+        } 
+    });
+}//end showForm
+});
+
+let checkBoxes = $('.activities input[type="checkbox"');           
+//if a box is checked
+checkBoxes.on('change', function(e) {
+    let $nextButton = $('#next');
+
+    //if nothing is checked, the button is disabled
+    if ($('.activities input:checkbox:checked').length === 0) {
+        $nextButton.attr('disabled', 'disabled');
+        $nextButton.css({"background-color": "gray"});
+    } else {
+        $nextButton.removeAttr('disabled');
+        $nextButton.css({"background-color" : "#22627e"});
+    }
+
+
+
+});
+
